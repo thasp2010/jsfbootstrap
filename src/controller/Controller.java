fb9c92b6b6d37b4aefca3ba5ad5ec864692e77a3
ThaisSilva <tha.sp2010@gmail.com>
2016-11-17 20:05:03 -0200

    Pagina de login JSF + Bootstrap

---
+++ src/controller/Controller.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name="controller")
+@RequestScoped
+public class Controller {
+	
+	private String nome;
+	private String login;
+	private String mensagem;
+	
+	public String enviarMensagem(){
+		return "index.xhtml";		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+
+
+	
+	
+
+}
